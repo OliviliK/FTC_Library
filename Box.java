d1c88c9bd490a74b1a8d843507cdcd26a0967259
Olavi Kamppari <OliviliK@users.noreply.github.com>
2015-11-16 18:38:47 -0500

    Create Box.java

---
+++ Box.java
@@ -0,0 +1,9 @@
+package org.usfirst.FTC5866.library;
+
+/**
+ * Created by Olavi Kamppari on 11/10/2015.
+ */
+class Box<AnyType> {
+    public      AnyType     value;
+    public Box( AnyType     value)  {this.value	    = value;}
+}
