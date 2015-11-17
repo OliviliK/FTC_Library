aa36ed2228ca44a80942da30b70a677d9210da89
Olavi Kamppari <OliviliK@users.noreply.github.com>
2015-11-16 20:02:13 -0500

    Update Box.java

--- Box.java
+++ Box.java
@@ -2,6 +2,8 @@ package org.usfirst.FTC5866.library;
 
 /**
  * Created by Olavi Kamppari on 11/10/2015.
+ * 
+ * Added to Github on 11/16/2015 (https://github.com/OliviliK/FTC_Library/blob/master/Box.java)
  */
 class Box<AnyType> {
     public      AnyType     value;
